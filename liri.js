5f5a0522b6246234db5e2d456e4d115619162fd4
Annie Pham <anniekpham@yahoo.com>
2019-04-25 21:17:41 -0700

    finshed app

--- liri.js
+++ liri.js
@@ -27,11 +27,10 @@ const spotifythis = track => {
     let keys = require("./keys.js")
     let spotify = new Spotify(keys.spotify)
     spotify.search({ type: 'track', query: track, limit: 1})
-    // can't desructure object
     .then((data) => {
         let artist = data.tracks.items[0].album.artists[0].name
         let song = data.tracks.items[0].name
-        let preview = data.tracks.items[0].preview_url
+        let preview = data.tracks.items[0].external_urls.spotify
         let album = data.tracks.items[0].album.name
         console.log(`
         Artist: ${artist}
@@ -41,8 +40,9 @@ const spotifythis = track => {
         `)
     })
     .catch(e => console.log(e))
-    fs.appendFile('log.txt', `${command},${title.join(' ')};`, e => e ? console.log(e): console.log())
+    fs.appendFile('log.txt', `${command},${title.join(' ')}; `, e => e ? console.log(e): console.log())
 }
+
 // search movie title
 const moviethis = movie => {
     axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=d467d7fe`)
@@ -57,7 +57,7 @@ const moviethis = movie => {
     Actors: ${Actors}
     `))
     .catch(e => console.log(e, "Please Enter a Valid Title"))
-    fs.appendFile('log.txt', `${command},${title.join(' ')};`, e => e ? console.log(e): console.log())
+    fs.appendFile('log.txt', `${command},${title.join(' ')}; `, e => e ? console.log(e): console.log())
 
 }
 
@@ -70,21 +70,27 @@ const concertthis = artist => {
     Date: ${format(datetime)}
     `))
     .catch(e => console.log(e, "This artist has no upcoming event"))
-    fs.appendFile('log.txt', `${command},${title.join(' ')};`, e => e ? console.log(e): console.log())
+    fs.appendFile('log.txt', `${command},${title.join(' ')}; `, e => e ? console.log(e): console.log())
 
 }
 
 switch (command) {
     case 'movie-this':
-        // can't get it to run Mr. nobody when no title
-        moviethis(title.join(' '))
+        if (title.length === 0){
+            moviethis('Mr. Nobody')
+        } else {
+            moviethis(title.join(' '))
+        }
     break
     case 'concert-this':
         concertthis(title.join(' '))
     break
     case 'spotify-this-song':
-        // can't get it to run The Sign when no title
-        spotifythis(title.join(' '))
+        if (title.length === 0){
+            spotifythis('The Sign')
+        } else {
+            spotifythis(title.join(' '))
+        }
     break
     case 'do-what-it-says':
         fs.readFile('random.txt', 'utf8', (e, data) => e ? console.log(e) : random(data))
